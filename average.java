32848e3d0d2c7ac3e7a880ae66a6d659e1188ef8
Sonal Mittal <sonalmittal140103@gmail.com>
2025-02-04 23:01:06 +0530

    Average of 3 Number

---
+++ average.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class average {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Number : ");;
+        int a = scanner.nextInt();
+        System.out.println("Enter Second Number : ");;
+        int b = scanner.nextInt();
+        System.out.println("Enter Third Number : ");;
+        int c = scanner.nextInt();
+
+        double avg = (a+b+c)/3;
+        System.out.println("Average is : "+avg);
+
+        scanner.close();
+    }
+}
